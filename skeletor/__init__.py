dcad124f8fb7035897c87fbfce16e3d92b80f25b
Philipp Schlegel <observing@web.de>
2020-07-16 12:29:37 +0100

    swap version/verstr

--- skeletor/__init__.py
+++ skeletor/__init__.py
@@ -16,8 +16,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.
 
-__version__ = (0, 2, 1)
-__verstr__ = "0.2.1"
+__version__ = "0.2.1"
+__version_vector__ = (0, 2, 1)
 
 from .meshcontraction import contract
 from .skeletonizers import skeletonize
