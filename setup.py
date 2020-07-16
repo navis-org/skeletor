7a10f5d1d257a1709dd0ffb637ce0a6cf67412aa
Philipp Schlegel <observing@web.de>
2020-07-16 12:31:12 +0100

    bump required Python versions

--- setup.py
+++ setup.py
@@ -39,10 +39,11 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=requirements,
-    python_requires='>=3.3',
+    python_requires='>=3.6',
     zip_safe=False
 )
