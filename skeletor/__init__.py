41ca9f1c8170c77443125fe5a5df8f5dd593aeba
Philipp Schlegel <observing@web.de>
2020-10-13 10:36:49 +0100

    bump version 0.2.8 -> 0.2.9

--- skeletor/__init__.py
+++ skeletor/__init__.py
@@ -16,8 +16,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.
 
-__version__ = "0.2.8"
-__version_vector__ = (0, 2, 8)
+__version__ = "0.2.9"
+__version_vector__ = (0, 2, 9)
 
 from .meshcontraction import contract
 from .skeletonizers import skeletonize
