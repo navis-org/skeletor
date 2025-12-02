e724f2dc23c8824ef396e973881b89b1516fe866
Philipp Schlegel <observing@web.de>
2025-12-02 16:14:48 +0000

    bump version 1.3.1 -> 1.4.0

--- skeletor/__init__.py
+++ skeletor/__init__.py
@@ -263,8 +263,8 @@ tucked away into submodules (see side-bar or above table).
 
 """
 
-__version__ = "1.3.1"
-__version_vector__ = (1, 3, 1)
+__version__ = "1.4.0"
+__version_vector__ = (1, 4, 0)
 
 from . import skeletonize
 from . import pre
